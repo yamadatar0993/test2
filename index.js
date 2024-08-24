99485e07c39e0ce0f4b6b12fe02c48ad5bd9609d
yamadataro <hofjadoies@gmail.com>
2024-08-25 03:58:32 +0900

    htmlファイルを読み込んで表示するように変更

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
